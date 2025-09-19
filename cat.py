0089f16625d62d066d30a702777a6c48ce23793e
knilfix <knilfix360@gmail.com>
2025-09-19 17:43:00 +0300

    improved cat

--- cat.py
+++ cat.py
@@ -2,4 +2,12 @@ print("I am a cat")
 
 
 def meow():
-    return "I can Meow Now"
+    return "I can Meow well Now"
+
+
+def run():
+    return "I can Run well now"
+
+
+def die():
+    return "Cat Died"
