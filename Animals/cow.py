078ac7bde35f6fb3de5d21733feaddc7e2528c58
knilfix <knilfix360@gmail.com>
2025-09-19 19:02:32 +0300

    added cow

---
+++ Animals/cow.py
@@ -0,0 +1,12 @@
+from animal import Animal
+
+
+class Cow(Animal):
+    def __init__(self, name):
+        super().__init__(name, "Bos taurus")
+
+    def speak(self):
+        return "Moo!"
+
+    def give_milk(self):
+        return f"{self.name} is giving milk"
